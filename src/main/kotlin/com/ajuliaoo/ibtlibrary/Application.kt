10d578d8e6f669b296970c3f291965f8f9af2c27
Artur Bruno <arturjuliao20@gmail.com>
2025-03-11 22:00:03 -0300

    feat: initial project

---
+++ src/main/kotlin/com/ajuliaoo/ibtlibrary/Application.kt
@@ -0,0 +1,13 @@
+package com.ajuliaoo.ibtlibrary
+
+import io.ktor.server.application.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>) {
+    EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
