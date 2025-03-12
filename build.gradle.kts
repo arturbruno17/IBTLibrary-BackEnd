90f7d96d47a91ea932a752cc64357fa87fc2fe1d
Artur Bruno <arturjuliao20@gmail.com>
2025-03-11 23:01:15 -0300

    build: add libs to integrate database

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
-
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val h2_version: String by project
+val postgres_version: String by project
 
 plugins {
     kotlin("jvm") version "2.1.10"
@@ -27,6 +29,12 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation")
     implementation("io.ktor:ktor-serialization-kotlinx-json")
     implementation("io.ktor:ktor-server-netty")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
+    implementation("io.ktor:ktor-server-host-common")
+    implementation("io.ktor:ktor-server-status-pages")
+    implementation("org.postgresql:postgresql:$postgres_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
     testImplementation("io.ktor:ktor-server-test-host")
