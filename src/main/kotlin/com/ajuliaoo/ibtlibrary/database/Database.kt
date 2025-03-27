7ca5b5d680fbc1886970349a9104f3ee2ce6c537
Artur Bruno <arturjuliao20@gmail.com>
2025-03-27 20:32:37 -0300

    feat: create route to get all loans
    - Set server timezone to UTC
    - Create a new Loan property called type, it can be IN_DAYS, OVERDUE and RETURNED
    - Create a new route to get all loans
      - The route can filter by a list of Loan.Type, book_id and person_id

--- src/main/kotlin/com/ajuliaoo/ibtlibrary/database/Database.kt
+++ src/main/kotlin/com/ajuliaoo/ibtlibrary/database/Database.kt
@@ -2,8 +2,10 @@ package com.ajuliaoo.ibtlibrary.database
 
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.Database
+import java.util.TimeZone
 
 fun Application.configureDatabase() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     Database.connect(
         url = environment.config.property("ktor.database.url").getString(),
         user = environment.config.property("ktor.database.user").getString(),
