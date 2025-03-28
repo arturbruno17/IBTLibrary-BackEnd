c3ab63ae3389b25d13393aa50e506d6e206e09da
Artur Bruno <arturjuliao20@gmail.com>
2025-03-27 21:16:10 -0300

    feat: create route to get all person's loans
    Only librarian or own person can access this route.

--- src/main/kotlin/com/ajuliaoo/ibtlibrary/repositories/loan/LoanRepository.kt
+++ src/main/kotlin/com/ajuliaoo/ibtlibrary/repositories/loan/LoanRepository.kt
@@ -3,7 +3,7 @@ package com.ajuliaoo.ibtlibrary.repositories.loan
 import com.ajuliaoo.ibtlibrary.models.Loan
 
 interface LoanRepository {
-    suspend fun getAllLoans(bookId: Int?, personId: Int?, types: List<Loan.Type>?): List<Loan>
+    suspend fun getAllLoans(bookId: Int? = null, personId: Int? = null, types: List<Loan.Type>? = null): List<Loan>
     suspend fun createLoan(personId: Int, bookId: Int): Loan
     suspend fun activeLoansByBookId(bookId: Int): List<Loan>
     suspend fun returnBook(loanId: Int): Loan?
